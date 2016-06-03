6119f89bdf85bbdba46f878417606396458e1939
Loveskyrim <dr.konovalov@gmail.com>
2016-06-03 21:36:25 +0300

    Add files via upload

--- sotrudnik.cpp
+++ sotrudnik.cpp
@@ -1,8 +1,10 @@
 #include "sotrudnik.h"
-#include <iostream>
+#include "podrazdel.h"
+
 
 sotrudnik::sotrudnik(void)
 {
+	stcom=rand() % 4;// Командировка
 }
 
 sotrudnik::sotrudnik(string fio1)
@@ -10,13 +12,23 @@ sotrudnik::sotrudnik(string fio1)
 	fio=fio1;
 //	oklad=oklad1;
 	//pPodr=p;
+	stcom=rand() % 4;// Командировка
 }
 
 	void sotrudnik::Print()
 	{
-		cout<< endl << "Name Surname" << fio << "oklad" ;/////
+		cout<<  "FIO: " << fio << " oklad="<< oklad<<endl;/////
+	}
+
+	void sotrudnik::PrintInFile(ostream& f) 
+	{
+		f<< fio << endl<<oklad<<endl;/////
 	}
 	
+	void sotrudnik::readFromFile(istream& f)
+	{
+		f>>fio>>oklad;
+	}
 	double sotrudnik::GetOklad()
 	{
 		return oklad;
@@ -32,13 +44,37 @@ sotrudnik::sotrudnik(string fio1)
 		return stcom;
 	}
 
-	sotrudnik::sotrudnik(string name1, double oklad1, podrazdel *p)
+	sotrudnik::sotrudnik(string name1, double oklad1, podrazdel *p) // Конструктор включает сотрудника в подразделение
 		{
 			fio=name1;
 			oklad=oklad1;
 			pPodr=p;
 			pPodr->listst.push_back(*this);
+			stcom=rand() % 4;// Командировка
+			if (rand() % 2) prem=true;
+			else prem=false;
 		}
+	bool sotrudnik::operator==(const sotrudnik& s2)
+	{
+		return fio==s2.fio;
+	}
+	string& sotrudnik::GetFio()
+	{
+		return fio;
+	}
+
+	sotrudnik::sotrudnik(string name1, double oklad1) // Конструктор создает сотрудника без подразделения
+		{
+			fio=name1;
+			oklad=oklad1;
+			stcom=rand() % 4;// Командировка
+			if (rand() % 2) prem=true;
+			else prem=false;
+		}
+	void sotrudnik::SetPrem(bool flag)
+	{
+		prem=flag;
+	}
 
 
 
@@ -52,6 +88,7 @@ sotrudnik::sotrudnik(string fio1)
 	void sotrudnik::SetPodr(podrazdel *p, ostream& out)
 	{
 		pPodr=p;
+		pPodr->listst.push_back(*this);
 		out << endl << "Сообщение об изменении" ;///////
 	}
 
